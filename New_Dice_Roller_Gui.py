c6b61df50938ffe873a2d06e937ce1c5efed45ea
Ryan Adams <uncletygg@yahoo.com>
2022-07-14 10:48:05 -0400

    Update New_Dice_Roller_Gui.py

--- New_Dice_Roller_Gui.py
+++ New_Dice_Roller_Gui.py
@@ -28,13 +28,13 @@ def button_click_clear():
 # Define Buttons
 button_1 = Button(window, text="d_2", padx=35, pady=20, command=lambda: button_click(2))
 button_2 = Button(window, text="d_4", padx=35, pady=20, command=lambda: button_click(4))
-button_3 = Button(window, text="d_6", padx=35, pady=20, command=lambda: button_click(6))
+button_3 = Button(window, text="d_6", padx=33, pady=20, command=lambda: button_click(6))
 button_4 = Button(window, text="d_8", padx=35, pady=20, command=lambda: button_click(8))
-button_5 = Button(window, text="d_10", padx=34, pady=20, command=lambda: button_click(10))
-button_6 = Button(window, text="d_12", padx=34, pady=20, command=lambda: button_click(12))
-button_7 = Button(window, text="d_20", padx=34, pady=20, command=lambda: button_click(20))
-button_8 = Button(window, text="d_100", padx=33, pady=20, command=lambda: button_click(100))
-button_9 = Button(window, text="Clear", padx=50, pady=20, command=button_click_clear)
+button_5 = Button(window, text="d_10", padx=32, pady=20, command=lambda: button_click(10))
+button_6 = Button(window, text="d_12", padx=30, pady=20, command=lambda: button_click(12))
+button_7 = Button(window, text="d_20", padx=32, pady=20, command=lambda: button_click(20))
+button_8 = Button(window, text="d_100", padx=29, pady=20, command=lambda: button_click(100))
+button_9 = Button(window, text="Clear", padx=29, pady=20, command=button_click_clear)
 
 # Put Buttons on screen
 button_1.grid(row=1, column=0)
@@ -45,7 +45,7 @@ button_5.grid(row=2, column=1)
 button_6.grid(row=2, column=2)
 button_7.grid(row=3, column=0)
 button_8.grid(row=3, column=1)
-button_9.grid(row=4, column=0)
+button_9.grid(row=3, column=2)
 
 # This tells Python to run the Tkinter event loop.
 window.mainloop()
